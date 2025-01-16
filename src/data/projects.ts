22717029e4417fff732f904e4886a7a17a52de9e
Tim Machnacki <machnackit@gmail.com>
2025-01-15 21:40:19 -0500

    add sharebase data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -15,6 +15,16 @@ import PodcastDetails from "@/assets/podvex/podcast-details.webp";
 import Profile from "@/assets/podvex/profile.webp";
 import Signup from "@/assets/podvex/signup.webp";
 
+// sharebase assets
+import Auth_SB from "@/assets/sharebase/auth.webp";
+import Create_SB from "@/assets/sharebase/create.webp";
+import Details_SB from "@/assets/sharebase/details.webp";
+import HomeDarkLight_SB from "@/assets/sharebase/home-dark-light.webp";
+import HomeDark_SB from "@/assets/sharebase/home-dark.webp";
+import HomeLight_SB from "@/assets/sharebase/home-light.webp";
+import Messages_SB from "@/assets/sharebase/messages.webp";
+import Profile_SB from "@/assets/sharebase/profile.webp";
+
 export const PROJECTS: ProjectProps[] = [
   {
     title: "Podvex",
@@ -126,6 +136,80 @@ export const PROJECTS: ProjectProps[] = [
       demo: "https://podvex.vercel.app/",
       source: "https://github.com/tmachnacki/podvex",
       details: "/projects/podvex",
+    },
+  },
+  {
+    title: "ShareBase",
+    date: "Aug. 2024",
+    description: "A modern social media platform",
+    features: [
+      "Light and dark modes",
+      "Responsive design",
+      "Real-time messaging",
+    ],
+    thumbnail: HomeDarkLight,
+    images: [
+      {
+        label: "Home",
+        image: HomeDarkLight_SB,
+        compare: true,
+        compareImages: {
+          labelOne: "Home (Dark)",
+          imageOne: HomeDark_SB,
+          labelTwo: "Home (Light)",
+          imageTwo: HomeLight_SB,
+        },
+      },
+      {
+        label: "Create Post",
+        image: Create_SB,
+      },
+      {
+        label: "Post Details",
+        image: Details_SB,
+      },
+      {
+        label: "Profile",
+        image: Profile_SB,
+      },
+      {
+        label: "Messages",
+        image: Messages_SB,
+      },
+      {
+        label: "Auth",
+        image: Auth_SB,
+      },
+    ],
+    skills: [
+      {
+        name: "TypeScript",
+      },
+      {
+        name: "React",
+        description: "UI framework",
+      },
+      {
+        name: "Firebase",
+        description: "Document-based database",
+      },
+      {
+        name: "Tailwind CSS",
+        description: "Utility-first CSS framework",
+      },
+      {
+        name: "shadcn/ui",
+        description: "Components",
+      },
+      {
+        name: "Vercel",
+        description: "Hosting",
+      },
+    ],
+    actions: {
+      demo: "https://sharebase.vercel.app/",
+      source: "https://github.com/tmachnacki/sharebase",
+      details: "/projects/sharebase",
     },
   },
 ];
