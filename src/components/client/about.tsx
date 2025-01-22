8436c8503a29b7e9feacceed2b6f890db8135fbc
Timothy Machnacki <tmachnacki@signs365.com>
2025-01-22 16:15:34 -0500

    nav-links scrollspy, nav section positioning

--- src/components/client/about.tsx
+++ src/components/client/about.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import TypingText from "./typing-text";
-import { TypingAnimation } from "./typing-animation";
 
 const About = () => {
   const [showTypingText, setShowTypingText] = useState(false);
@@ -26,7 +25,7 @@ const About = () => {
   }, [beginGPTResponse]);
 
   return (
-    <div className="flex h-auto min-h-48 w-full flex-col gap-8 bg-red-500 px-4">
+    <div className="flex h-auto w-full flex-col gap-8 px-4">
       <div className="-me-4 w-full max-w-xs self-end rounded-2xl bg-muted px-4 py-2 text-muted-foreground sm:max-w-sm md:max-w-md lg:max-w-sm">
         {showTypingText ? (
           <TypingText
@@ -41,18 +40,12 @@ const About = () => {
         ) : (
           <p>Message ChatGPT</p>
         )}
-
-        {/* <TypingAnimation
-        delay={1000}
-        duration={50}
-        className="text-sm font-normal text-muted-foreground"
-      >{`Write me an "about" section for my frontend software engineer portfolio without using any jargon or buzzwords, like "passionate".`}</TypingAnimation> */}
       </div>
 
       <div
         className={`-ms-4 mr-4 w-full max-w-xs self-start rounded-2xl border bg-transparent px-4 py-2 text-muted-foreground sm:max-w-sm md:max-w-md lg:max-w-sm`}
       >
-        {showGPTResponse ? (
+        {showGPTResponse && (
           <TypingText
             text={`I build things for the web.`}
             className="w-full text-left"
@@ -61,10 +54,6 @@ const About = () => {
             hideCursorOnComplete={true}
             alwaysVisibleCount={0}
           />
-        ) : (
-          <p aria-hidden="true" className="h-6">
-            hidden placeholder
-          </p>
         )}
       </div>
     </div>
