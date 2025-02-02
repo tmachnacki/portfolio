5a878f7a29dd97462ffb487923992b8aecbfd643
Tim Machnacki <machnackit@gmail.com>
2025-02-02 18:43:38 -0500

    adjust skills, breadcrumb alignment, prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,9 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+  tailwindAttributes: ["class", "className"],
+  tailwindFunctions: ["cn", "cva"],
+  tailwindConfig: "./tailwind.config.mjs",
   overrides: [
     {
       files: "*.astro",
