e3c73e803f96ae106518a51a9692f6555fa8b27e
Tim Machnacki <machnackit@gmail.com>
2025-02-02 21:41:06 -0500

    experience, favicons

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -6,7 +6,8 @@ export interface ExperienceProps {
   jobType?: string;
   company: string;
   companyUrl?: string;
-  bullets: string[];
+  description?: string;
+  bullets?: string[];
   skills?: string[];
 }
 
@@ -17,12 +18,8 @@ export const EXPERIENCE: ExperienceProps[] = [
     jobType: "Full-Time",
     company: "Signs365",
     companyUrl: "https://signs365.com",
-    bullets: [
-      "Leading complete frontend architecture and development of new v5 application using React 19, integrating modern tools such as Tanstack (React) Query for advanced async state management and React Router v7 for client-side routing.",
-      "Designed and implemented a new t-shirt ordering interface, developing 35+ reusable and accessible UI components using React, TypeScript, and Tailwind CSS, while maintaining documentation through Storybook for team-wide adoption.",
-      "Decreased client chat network load by 66% with rendering optimization. Redesigned and implemented an improved chat user interface, adding proper handling of loading and error states, as well as infinite-scrolling message history.",
-      "Spearheaded the end-to-end design and development of a new “Promos” page within a one-week sprint, driving 1,300+ customer conversions through the successful launch of a 4th of July campaign.",
-    ],
+    description:
+      "Leading complete frontend architecture and development of new v5 site using React 19, Tanstack (React) Query, and React Router v7. Designed and implemented a new t-shirt ordering interface, developing 35+ reusable and accessible UI components using React, TypeScript, and Tailwind CSS, while maintaining documentation through Storybook for team-wide adoption. Work with backend engineers, designers, and management to develop and maintain application features.",
     skills: [
       "TypeScript",
       "React",
@@ -43,12 +40,9 @@ export const EXPERIENCE: ExperienceProps[] = [
     title: "Web Developer ·",
     jobType: "Freelance",
     company: "Recruiting Arena",
-    companyUrl: "https://google.com",
-    bullets: [
+    companyUrl: "https://recruitingarena.com/",
+    description:
       "Leveraged knowledge of React, TypeScript, and Tailwind CSS to build a UI component library",
-      "Did some other cool thing with some tech and code",
-      "Did another other cool thing with some tech and code",
-    ],
     skills: ["Astro", "Javascript", "HTML", "Tailwind CSS", "DaisyUI", "Figma"],
   },
   {
@@ -57,9 +51,8 @@ export const EXPERIENCE: ExperienceProps[] = [
     jobType: "Contract",
     company: "Buy My Spot",
     companyUrl: "https://www.buymyspot.com/",
-    bullets: [
+    description:
       "Delivered a streamlined 5-step user onboarding experience in collaboration with design leadership, implementing progressive multi-step form validation to enhance new user activation.",
-    ],
     skills: ["Typescript", "React Native", "Tailwind CSS", "Expo"],
   },
 ];
